9d6a32e63d89a6605b808080a4c4041d27300b3c
Kenny Szeto <kenny.szeto1@gmail.com>
2019-10-16 11:49:58 -0700

    set up environment, added smiley image, and background color

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World
+      <div className="how-steps-container">
+        <img src="https://sundae-takehome-project.s3-us-west-1.amazonaws.com/smiley-money.jpg"></img>
       </div>
     );
   }
