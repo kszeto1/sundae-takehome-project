4b5d856651e556c33a6cd69e8e12ac77f21c8973
Kenny Szeto <kenny.szeto1@gmail.com>
2019-10-16 12:45:17 -0700

    implemented button

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import ListSteps from './ListSteps.jsx';
+import MainButton from './MainButton.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="how-steps-container">
-        <img src="https://sundae-takehome-project.s3-us-west-1.amazonaws.com/smiley-money.jpg"></img>
-        <ListSteps />
+      <div>
+        <div className="how-steps-container">
+          <img src="https://sundae-takehome-project.s3-us-west-1.amazonaws.com/smiley-money.jpg"></img>
+          <ListSteps />
+        </div>
+        <div className="steps-button-container">
+          <MainButton />
+        </div>
       </div>
     );
   }
---
+++ client/src/components/MainButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MainButton = () => {
+  return (
+    <a className="main-button">GET OFFER</a>
+  )
+}
+
+export default MainButton;
